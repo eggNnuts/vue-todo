baa4e243ea22ef9383b5dd9f94e4ce5edb39afa5
Byoung Seong Lee <dev.bslee@gmail.com>
2021-05-26 23:54:09 +0900

    pug-loader(html 전처리기)및 vue-loader적용 ,  App.vue 구현

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello webpack")
+import {Vue} from 'vue';
+import {App} from "./App.vue"
+
+new Vue({
+    el:"#todo",
+    /* 
+       보통 vue는 singpage component방식으로 template javasript css가 한 파일에 존재하여
+        template(html)태그에 직접 vue instance을 연결시켜주지만 
+
+        webpack의 entry로 지정된 main.js에서 tempate(html)을 작성할수 없어 vue 내장 함수인 render함수를 통해
+        App.vue에 명시된 template에  vue instace를 연결시켜줌
+
+    */
+    render:createElement=> createElement(App)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const pathModule = require('path');
-
+const { VueLoaderPlugin } = require('vue-loader');
 module.exports={
     mode :'production',
     entry:{
         // __dirname => 현재파일의 위치(경로),node.js에서 제공하는 환경 전역변수
-        todo:path.join(__dirname,'main.js')
+        todo:pathModule.join(__dirname,'main.js')
     },
     output:{
         filename :'[name].js', // output file name => todo.js
-        path : path.join(__dirname,"dist")
+        path : pathModule.join(__dirname,"dist")
     },
-    module:{},
-    plugins:[]
+    module:{
+        rules:[
+           // vue-loader 추가 ,
+           {
+               test : /\.vue$/,
+               loader :'vue-loader'
+           },
+           // pug(jade라고도함,html 전처리기) loader 추가
+           {
+               test: /\.pug$/,
+               loader: 'pug-plain-loader'
+           }   
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ]
 }
